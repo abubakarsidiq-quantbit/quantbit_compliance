fcca235ddd172e5e8521c801e31e636cc8099ee8
tejalkumbhar2001 <tejalkumbhar5258@gmail.com>
2024-07-05 15:09:46 +0530

    Loan Intrest Calculator

---
+++ quantbit_compliance/quantbit_compliance/doctype/interest_calculator/interest_calculator.py
@@ -0,0 +1,37 @@
+import frappe
+from frappe.model.document import Document
+
+class InterestCalculator(Document):
+    @frappe.whitelist()
+    def get_chart(self):
+        try:
+            amount = self.amount
+            period_months = int(self.period) * 12
+            monthly_rate = (self.rate / 100) / 12
+            self.payment = self.calculate_pmt(monthly_rate, period_months, amount)
+            balance = amount
+            self.interest_chart.clear()
+            for month in range(1, int(period_months) + 1):
+                interest = balance * monthly_rate
+                principal = self.payment - interest
+                ending_balance = balance - principal
+                self.append('interest_chart', {
+                    'month': month,
+                    'beginning': balance,
+                    'payment': self.payment,
+                    'interest': interest,
+                    'principal': principal,
+                    'balance': ending_balance
+                })
+                balance = ending_balance
+
+        except Exception as e:
+            frappe.msgprint(f"Error generating interest chart: {str(e)}")
+            frappe.log_error(message=str(e), title="Interest Chart Generation Error")
+    def calculate_pmt(self,rate, nper, pv):
+        if rate != 0:
+            pmt = (rate * pv) / (1 - (1 + rate) ** -nper)
+        else:
+            pmt = pv / nper
+        return pmt
+
